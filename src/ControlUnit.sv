edfc46061deef570c1dab14d320f06f2d4979560
Mario1159 <mario@1159.cl>
2022-11-26 10:50:41 +0000

    Remove Vivado info

--- src/ControlUnit.sv
+++ src/ControlUnit.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 26.11.2022 04:31:59
-// Design Name: 
-// Module Name: ControlUnit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ControlUnit
 (
