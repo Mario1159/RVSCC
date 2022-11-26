99bd7296b719f8ca9aec08bde5d668b67b8672c5
Mario1159 <mario@1159.cl>
2022-11-26 10:52:17 +0000

    Remove Vivado info

--- src/DataMemory.sv
+++ src/DataMemory.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 26.11.2022 06:24:42
-// Design Name: 
-// Module Name: DataMemory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module DataMemory #(parameter N = 32)(
     input logic clk,
