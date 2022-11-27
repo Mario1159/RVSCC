e1136279a331539114e0c2128cc62a29cb8ee1f9
Mario Romero <mromero7242@gmail.com>
2022-11-27 03:32:55 -0300

    Add ROM

--- src/DataMemory.sv
+++ src/DataMemory.sv
@@ -1,10 +1,20 @@
 `timescale 1ns / 1ps
 
-module DataMemory #(parameter N = 32)(
+module DataMemory #(
+parameter N = 32,
+parameter SIZE = 64)(
     input logic clk,
     input logic[N-1:0] addr,
-    input logic enable_data,
+    input logic write_enable,
     input logic[N-1:0] write_data,
     output logic[N-1:0] read_data
 );
+    logic[N-1:0] mem[SIZE-1];
+    
+    always_ff @(posedge clk) begin
+        if (write_enable)
+            mem[addr] <= write_data;
+        else
+            read_data <= mem[addr];
+    end
 endmodule
