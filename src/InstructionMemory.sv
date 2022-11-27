f895a4ccf6e47aac761034bfa7d803cef3b8ca50
Mario Romero <mromero7242@gmail.com>
2022-11-27 03:30:54 -0300

    Describe instruction memory based on hex file

--- src/InstructionMemory.sv
+++ src/InstructionMemory.sv
@@ -1,9 +1,15 @@
 `timescale 1ns / 1ps
 
 // N = Bit width
-module InstructionMemory #(parameter N = 8)
+module InstructionMemory #(
+    parameter N = 32,
+    parameter SIZE = 32
+)
 (
     input logic[N-1:0] addr,
     output logic[N-1:0] instr
- );
+);
+    logic[N-1:0] mem [SIZE-1:0];
+
+    initial $readmemh("../build/program.hex", mem);
 endmodule
