cc3c30df260298a4a5b892ceb58f087ee105d285
Mario Romero <mario@1159.cl>
2023-01-30 01:41:58 -0300

    Update toolchain detection

--- fw/cmake/toolchain.cmake
+++ fw/cmake/toolchain.cmake
@@ -1,11 +1,15 @@
 set(CMAKE_SYSTEM_NAME Generic)
 
-if (UNIX)
-    set(TOOLCHAIN_PREFIX riscv32-unknown-linux-gnu-)
-endif()
-if (WIN32)
-    set(TOOLCHAIN_PREFIX riscv-none-elf-)
-endif()
+find_program(RISCV_GCC_FOUND
+  NAMES riscv-none-elf-gcc riscv32-unknown-linux-gnu-gcc)
+
+get_filename_component(GCC_BIN ${RISCV_GCC_FOUND} NAME)
+
+message("msg gcc: ${GCC_BIN}")
+
+string(REPLACE gcc "" TOOLCHAIN_PREFIX ${GCC_BIN})
+
+message("msg toolchain: ${TOOLCHAIN_PREFIX}")
 
 set(CMAKE_C_COMPILER   ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
