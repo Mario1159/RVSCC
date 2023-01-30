e826f9ff88a89df680623cd52134a5e07d5dbeec
Mario Romero <mario@1159.cl>
2023-01-30 02:23:11 -0300

    Update toolchain

--- fw/cmake/toolchain.cmake
+++ fw/cmake/toolchain.cmake
@@ -5,11 +5,7 @@ find_program(RISCV_GCC_FOUND
 
 get_filename_component(GCC_BIN ${RISCV_GCC_FOUND} NAME)
 
-message("msg gcc: ${GCC_BIN}")
-
 string(REPLACE gcc "" TOOLCHAIN_PREFIX ${GCC_BIN})
-
-message("msg toolchain: ${TOOLCHAIN_PREFIX}")
 
 set(CMAKE_C_COMPILER   ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
