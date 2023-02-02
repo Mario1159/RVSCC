71dd36c2876c06ee2fae214514e598779d403d85
Mario Romero <mario@1159.cl>
2023-02-02 19:46:17 -0300

    Organize main build

--- fw/cmake/riscv-toolchain.cmake
+++ fw/cmake/riscv-toolchain.cmake
@@ -1,12 +1,13 @@
 set(CMAKE_SYSTEM_NAME Generic)
 
 find_program(RISCV_GCC_FOUND
-  NAMES riscv-none-elf-gcc riscv32-unknown-linux-gnu-gcc)
+  NAMES riscv-none-elf-gcc riscv32-unknown-elf-gcc)
 
 get_filename_component(GCC_BIN ${RISCV_GCC_FOUND} NAME)
 
 string(REPLACE gcc "" TOOLCHAIN_PREFIX ${GCC_BIN})
 
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}as)
 set(CMAKE_C_COMPILER   ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
 set(CMAKE_OBJCOPY      ${TOOLCHAIN_PREFIX}objcopy)
---
+++ fw/sandbox/asm/main.s
@@ -0,0 +1,7 @@
+.section .text
+.global main
+
+main:
+  addi t0, t0, 3
+  addi t1, t1, 4
+  add a0, t1, t0
