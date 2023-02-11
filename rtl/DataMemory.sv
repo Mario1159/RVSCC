d06dcc6ceb38ab4ee4ecb23fbe0adb37b7077b58
Mario Romero <mario@1159.cl>
2023-02-11 19:29:27 -0300

    Add Data Memory test

--- rtl/DataMemory.sv
+++ rtl/DataMemory.sv
@@ -1,16 +1,16 @@
 `timescale 1ns / 1ps
 
 module DataMemory #(
-parameter N = 32,
-parameter SIZE = 32,
-parameter BYTE_WIDTH = 8)(
+parameter int N = 32,
+parameter int SIZE = 32,
+parameter int BYTE_WIDTH = 8)(
     input logic clk, rst,
     input logic[N-1:0] addr,
     input logic write_enable,
     input logic[N-1:0] write_data,
     output logic[N-1:0] read_data
 );
-    logic[BYTE_WIDTH-1:0] mem[SIZE*BYTE_WIDTH-1:0];
+    logic[SIZE*BYTE_WIDTH-1:0][BYTE_WIDTH-1:0] mem;
 
     assign read_data = {mem[addr + 'd0],
                         mem[addr + 'd1],
@@ -18,7 +18,7 @@ parameter BYTE_WIDTH = 8)(
                         mem[addr + 'd3]};
     always_ff @(posedge clk) begin
         if (rst)
-            mem = '{default: '0};
+            mem <= '{default: '0};
         else if (write_enable)
             {mem[addr + 'd0],
              mem[addr + 'd1],
--- test/Test_DataMemory.sv
+++ test/Test_DataMemory.sv
@@ -1,36 +1,54 @@
 `timescale 1ns / 1ps
 
-module Test_DataMemory();
-    logic clk, rst;
-    logic[31:0] addr;
-    logic write_enable;
-    logic[31:0] write_data;
-    logic[31:0] read_data;
-    
-    DataMemory #(
-        .SIZE(16)
-    ) data_memory(
-        clk,
-        rst,
-        addr,
-        write_enable,
-        write_data,
-        read_data
-    );
-    
-    always #1 clk = ~clk;
-    initial begin
-        clk = 0;
-        rst = 1;
-        write_enable = 0;
-        #4;
-        rst = 0;
-        #1;
-        write_enable = 1;
-        for(int i = 0; i < 16; i++) begin
-            addr = $urandom_range(15);
-            write_data = $urandom();
-            #2;
-        end
+module Test_DataMemory ();
+  logic clk, rst;
+  logic [31:0] addr;
+  logic write_enable;
+  logic [31:0] write_data;
+  logic [31:0] read_data;
+
+  localparam int MemorySize = 16;
+  DataMemory #(
+      .SIZE(MemorySize)
+  ) data_memory (
+      .clk(clk),
+      .rst(rst),
+      .addr(addr),
+      .write_enable(write_enable),
+      .write_data(write_data),
+      .read_data(read_data)
+  );
+
+  always #1 clk = ~clk;
+
+  localparam int MemoryWriteRange = 16;
+
+  logic [MemoryWriteRange:0][31:0] write_values;
+  int start_addr;
+  initial begin
+    // Reset
+    clk = 0;
+    rst = 1;
+    write_enable = 0;
+    #4;
+    rst = 0;
+    #1;
+    // Write to a range of values in memory
+    write_enable = 1;
+    start_addr = $urandom_range(15);
+    for (int i = 0; i < MemoryWriteRange; i++) begin
+      addr = start_addr + i;
+      write_values[i] = $urandom();
+      write_data = write_values[i];
+      #2;
     end
+    // Read and compare the same range of values
+    write_enable = 0;
+    for (int i = 0; i < 16; i++) begin
+      addr = start_addr + i;
+      assert (read_data == write_values[i])
+      else $error("Read failed at address %h", addr);
+    end
+    $finish;
+  end
 endmodule
