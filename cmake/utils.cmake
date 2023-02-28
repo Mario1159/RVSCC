1b18858d5f28c64beece6c9d5b94ec27b01d3c4f
Mario Romero <mario@1159.cl>
2023-02-27 23:26:17 -0300

    Rename include folder

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -44,7 +44,7 @@ function(rvscc_add_test)
 	PREFIX verilator_${TEST_TOP}
 	TOP_MODULE ${TEST_TOP}
 	TRACE
-	VERILATOR_ARGS --timing --assert -I${PROJECT_SOURCE_DIR}/hwinc
+	VERILATOR_ARGS --timing --assert -I${PROJECT_SOURCE_DIR}/include
   )
   target_compile_definitions(${TEST_TARGET_NAME} PRIVATE
     TEST_HEADER="verilator_${TEST_TOP}.h"
